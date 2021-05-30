a591e107a18cd290b591f2355da707430aedcb10
Talykov Nikita <talykovnikita@gmail.com>
2021-05-30 23:25:40 +0300

    Add generation of test data with faker

--- src/test/java/tests/RegistrationFormTests.java
+++ src/test/java/tests/RegistrationFormTests.java
@@ -1,14 +1,16 @@
 package tests;
 
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+
 import hobby.Hobby;
 import gender.Gender;
 import student.Student;
 import pages.*;
 
-public class RegistrationFormTests extends TestBase{
+public class RegistrationFormTests extends TestBase {
 
     @Test
     void successfulRegistration() {
@@ -17,10 +19,32 @@ public class RegistrationFormTests extends TestBase{
                 "male@email.com",
                 Gender.Male,
                 "9998887776",
-                "15.03.1995",
+                "15.03.2000",
                 Arrays.asList("Computer Science", "Physics"),
                 Arrays.asList(Hobby.Sports, Hobby.Music),
                 "Male address 123 123",
+                "src/test/resources/logo.png",
+                "Haryana",
+                "Panipat"
+        );
+
+        registerStudent(student);
+        ResultTable.verifyInputDataForStudent(student);
+    }
+
+    @Test
+    void successfulRegistrationWithFaker() {
+        Faker testDataProvider = new Faker();
+
+        Student student = new Student(testDataProvider.name().firstName(),
+                testDataProvider.name().lastName(),
+                testDataProvider.internet().emailAddress(),
+                Gender.Male,
+                testDataProvider.numerify("##########"),
+                "15.03.2000",
+                Arrays.asList("Computer Science", "Physics"),
+                Arrays.asList(Hobby.Sports, Hobby.Music),
+                testDataProvider.address().fullAddress(),
                 "src/test/resources/logo.png",
                 "Haryana",
                 "Panipat"

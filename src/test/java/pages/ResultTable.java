5a26f3235161d0663eaab7e188502e6f43111751
Talykov Nikita <talykovnikita@gmail.com>
2021-05-30 22:56:09 +0300

    Change $ to $x

--- src/test/java/pages/ResultTable.java
+++ src/test/java/pages/ResultTable.java
@@ -6,14 +6,14 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Locale;
+
 import hobby.Hobby;
 import gender.Gender;
 import student.Student;
 
-import static com.codeborne.selenide.Selectors.byXpath;
-import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
 
-public  class ResultTable {
+public class ResultTable {
     private static String nameXpath = "//tbody//td[text()='Student Name']//..//td[2]";
     private static String emailXpath = "//tbody//td[text()='Student Email']//..//td[2]";
     private static String genderXpath = "//tbody//td[text()='Gender']//..//td[2]";
@@ -25,7 +25,7 @@ public  class ResultTable {
     private static String addressXpath = "//tbody//td[text()='Address']//..//td[2]";
     private static String cityAndStateXpath = "//tbody//td[text()='State and City']//..//td[2]";
 
-    public static void verifyInputDataForStudent(Student student){
+    public static void verifyInputDataForStudent(Student student) {
         isCorrectStudentNamePresent(student.firstName + ' ' + student.lastName);
         isCorrectStudentEmailPresent(student.email);
         isCorrectGenderPresent(student.gender);
@@ -38,28 +38,28 @@ public  class ResultTable {
         isCorrectCityAndStatePresent(student.city, student.state);
     }
 
-    private static void isCorrectStudentNamePresent(String name){
-        $(byXpath(nameXpath)).shouldHave(Condition.text(name));
+    private static void isCorrectStudentNamePresent(String name) {
+        $x(nameXpath).shouldHave(Condition.text(name));
     }
 
-    private static void isCorrectStudentEmailPresent(String email){
-        $(byXpath(emailXpath)).shouldHave(Condition.text(email));
+    private static void isCorrectStudentEmailPresent(String email) {
+        $x(emailXpath).shouldHave(Condition.text(email));
     }
 
-    private static void isCorrectGenderPresent(Gender gender){
-        $(byXpath(genderXpath)).shouldHave(Condition.text(gender.name()));
+    private static void isCorrectGenderPresent(Gender gender) {
+        $x(genderXpath).shouldHave(Condition.text(gender.name()));
     }
 
-    private static void isCorrectStudentMobilePresent(String phone){
-        $(byXpath(mobileXpath)).shouldHave(Condition.text(phone));
+    private static void isCorrectStudentMobilePresent(String phone) {
+        $x(mobileXpath).shouldHave(Condition.text(phone));
     }
 
-    private static void isCorrectDateOfBirthPresent(String dateOfBirth){
+    private static void isCorrectDateOfBirthPresent(String dateOfBirth) {
         String convertedDate = convertDateToOutputFormat(dateOfBirth);
-        $(byXpath(dateOfBirthXpath)).shouldHave(Condition.text(convertedDate));
+        $x(dateOfBirthXpath).shouldHave(Condition.text(convertedDate));
     }
 
-    private static String convertDateToOutputFormat(String date){
+    private static String convertDateToOutputFormat(String date) {
         DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("dd MMMM,yyyy", Locale.US);
 
@@ -67,27 +67,27 @@ public  class ResultTable {
         return outputDate.format(outputFormatter);
     }
 
-    private static void isCorrectSubjectsPresent(List<String> subjects){
-        for (String subject:subjects){
-            $(byXpath(subjectsXpath)).shouldHave(Condition.text(subject));
+    private static void isCorrectSubjectsPresent(List<String> subjects) {
+        for (String subject : subjects) {
+            $x(subjectsXpath).shouldHave(Condition.text(subject));
         }
     }
 
-    private static void isCorrectHobbiesPresent(List<Hobby> hobbies){
-        for (Hobby hobby:hobbies){
-            $(byXpath(hobbiesXpath)).shouldHave(Condition.text(hobby.name()));
+    private static void isCorrectHobbiesPresent(List<Hobby> hobbies) {
+        for (Hobby hobby : hobbies) {
+            $x(hobbiesXpath).shouldHave(Condition.text(hobby.name()));
         }
     }
 
-    private static void isCorrectPicturePresent(String path){
-        $(byXpath(pictureXpath)).shouldHave(Condition.text(path.split("/")[path.split("/").length-1]));
+    private static void isCorrectPicturePresent(String path) {
+        $x(pictureXpath).shouldHave(Condition.text(path.split("/")[path.split("/").length - 1]));
     }
 
-    private static void isCorrectAddressPresent(String address){
-        $(byXpath(addressXpath)).shouldHave(Condition.text(address));
+    private static void isCorrectAddressPresent(String address) {
+        $x(addressXpath).shouldHave(Condition.text(address));
     }
 
     private static void isCorrectCityAndStatePresent(String city, String state) {
-        $(byXpath(cityAndStateXpath)).shouldHave(Condition.text(state + ' ' + city));
+        $x(cityAndStateXpath).shouldHave(Condition.text(state + ' ' + city));
     }
 }
